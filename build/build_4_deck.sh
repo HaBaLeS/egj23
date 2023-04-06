e8d3d0c9f244fa00c6f46747f9e2fea816c96d9b
Falko Lehmann <falko@lehmann-carpzov.de>
2023-04-06 16:05:35 +0200

    ser version to v0.0.1
    Signed-off-by: Falko Lehmann <falko@lehmann-carpzov.de>

--- build/build_4_deck.sh
+++ build/build_4_deck.sh
@@ -1,5 +1,4 @@
 GO_VERSION=1.20.3
-VERSION=0.0.1
 STEAM_RT=registry.gitlab.steamos.cloud/steamrt/scout/sdk:latest
 
 if test -f "go$GO_VERSION.linux-amd64.tar.gz"; then
@@ -19,5 +18,5 @@ export PATH=$PATH:/tmp/go/bin/
 export GOCACHE=/tmp/
 export GOPATH=/tmp/go
 export CGO_CFLAGS=-std=gnu99
-go build -ldflags \"-X main.version=$VERSION -X main.buildtime=`date +%Y-%m-%d@%H:%M:%S`\"  -o bin/steam_egj23
+go build -ldflags \"-X main.version=$1 -X main.buildtime=`date +%Y-%m-%d@%H:%M:%S`\"  -o bin/steam_egj23
 "
